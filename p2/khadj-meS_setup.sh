ca373eab5e08daada7ca3526444c021026f02c35
Khalil Hadj Messaoud <khalilhadjmes1@gmail.com>
2026-03-27 16:05:06 +0100

    changed p2 because missed all other urls going to app3.com as said in the subject

--- p2/khadj-meS_setup.sh
+++ p2/khadj-meS_setup.sh
@@ -1,19 +1,18 @@
 #!/bin/bash
 
 # Exporting Controle-plane IP
-export CONTROL_PLANE_IP=192.168.56.110
-echo "[PROVISION-LOG] Exported Control-plane IP -> CP_IP = $CONTROL_PLANE_IP"
+CONTROL_PLANE_IP=192.168.56.110
 
 # Install K3s on the master node
 curl -sfL https://get.k3s.io | sh -s - --node-ip $CONTROL_PLANE_IP
 
 # Make sure kubectl is set up for the vagrant user
-echo "[PROVISION-LOG] Making kubectl not sudo only"
 sudo mkdir -p /home/vagrant/.kube
 sudo cp /etc/rancher/k3s/k3s.yaml /home/vagrant/.kube/config
 sudo chown -R vagrant:vagrant /home/vagrant/.kube/config \
 				/etc/rancher/k3s/k3s.yaml
 sudo chmod 644 /etc/rancher/k3s/k3s.yaml
+echo "[PROVISION-LOG] Maked kubectl not sudo only"
 
 # Copying from source files
 cp /vagrant/apps.yaml ./apps.yaml
